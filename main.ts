38ca7fa0b51de49452acb53cdc0d8685e12a22e3
Sam Rose <hello@samwho.dev>
2024-06-01 21:35:56 +0100

    Correct old link setting.

--- main.ts
+++ main.ts
@@ -82,7 +82,7 @@ class WikidataEntitySuggestModal extends SuggestModal<Entity> {
 	}
 
 	async onChooseSuggestion(item: Entity, evt: MouseEvent | KeyboardEvent) {
-		let loading = new Notice(`Importing entity ${item.id}...`);
+		let loading = new Notice(`wtf? bbq? Importing entity ${item.id}...`);
 
 		try {
 			if (this.plugin.settings.internalLinkPrefix === "db/") {
@@ -155,6 +155,8 @@ export default class WikidataImporterPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+
+		console.log("WikidataImporterPlugin loaded");
 
 		this.addCommand({
 			id: "import-properties-for-active-file",
